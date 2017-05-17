0c1dfeb63fc621d8f8a9473c88857b535d0f636f
524855 <annguyen@C02S7148FVH8.home>
2017-05-17 18:47:13 -0400

    moved everything from -humanity_app_frontend to  humanity_app_frontend

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express();
+var port = 8000 || process.env.PORT;;
+
+app.use(express.static('public'))
+
+
+app.listen(port, function(){
+  console.log('=================================================');
+  console.log('Humanity App frontend server runs on port: ', port);
+  console.log('=================================================');
+})
