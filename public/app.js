1f5bc81b2619de2966959dc88de6d006d69a7549
Soniya Jassal <jassal_soniya@bah.com>
2017-05-18 03:39:18 -0400

    Front End WireFrame Set Up

---
+++ public/app.js
@@ -0,0 +1,59 @@
+$(funtion(){
+   console.log('Jquery Loaded');
+
+   // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+});
+
+var app = angular.modue('CAHApp', ['ngRoute']);
+
+app.controller('CardsController', ['$http', function($http){
+   this.landing = false;
+
+   // //GET ALL NOTICES
+   // $http({
+   //    method: 'GET',
+   //    url: 'http://localhost:3000/notices'
+   // }).then(function(result){
+   //    console.log(result);
+   //    this.notices = result.data;
+   //
+   // }.bind(this));
+
+
+}]);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+   $locationProvider.html5Mode({enabled:true});
+   $routeProvider.when('/url1', { //route would come from controller file with routes
+      template: '<h2>This is the the URL1 Section</h2>',
+      controller: function(){
+         this.foo = 'bar';
+      },
+      controllerAs: 'main'
+   });
+}]);
