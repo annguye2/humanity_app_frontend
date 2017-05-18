7597045ab8c57d3a5314588d946bc9e50c5791d4
524855 <annguyen@C02S7148FVH8.home>
2017-05-18 12:17:27 -0400

    angular error

--- public/app.js
+++ public/app.js
@@ -35,27 +35,27 @@ var app = angular.module('CAHApp', []);
 
 app.controller('CardsController', ['$http', function($http){
 
-   this.blackcards =[];
+//   this.blackcards =[];
    //GET ALL WHITE CARDS
-   $http({
-      method: 'GET',
-      url: 'http://localhost:3000/blackcards'
-   }).then(function(result){
-      console.log(result.data);
-      this.blackcards = result.data;
-      console.log(this.blackcards);
-   }.bind(this));
+  //  $http({
+  //     method: 'GET',
+  //     url: 'http://localhost:3000/blackcards'
+  //  }).then(function(result){
+  //     console.log(result.data);
+  //     this.blackcards = result.data;
+  //     console.log(this.blackcards);
+  //  }.bind(this));
 
 
 }]);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-   $locationProvider.html5Mode({enabled:true});
-   $routeProvider.when('/url1', { //route would come from controller file with routes
-      template: '<h2>This is the the URL1 Section</h2>',
-      controller: function(){
-         this.foo = 'bar';
-      },
-      controllerAs: 'main'
-   });
-}]);
+// app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+//    $locationProvider.html5Mode({enabled:true});
+//    $routeProvider.when('/url1', { //route would come from controller file with routes
+//       template: '<h2>This is the the URL1 Section</h2>',
+//       controller: function(){
+//          this.foo = 'bar';
+//       },
+//       controllerAs: 'main'
+//    });
+// }]);
