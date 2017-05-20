869443786a4e0b1961bbfa58c89831983ef4bcc9
524855 <annguyen@C02S7148FVH8.home>
2017-05-20 16:05:49 -0400

    fixing server.js: remove unuse line codes that cause server errors

--- server.js
+++ server.js
@@ -2,45 +2,44 @@
 //require modules and models
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var methodOverride = require ('method-override');
-app.use(bodyParser.json());
+// var session = require('express-session');
+// var methodOverride = require ('method-override');
+// app.use(bodyParser.json());
+// var bcrypt= require('bcrypt');
 
-var bcrypt= require('bcrypt');
+
 app.use(express.static('public'));
-
 var port = 8000 || process.env.PORT;
 
-
-
-app.use(methodOverride('_method'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(session({
-    secret: "feedmeseymour", //some random string
-    resave: false,
-    saveUninitialized: false
-}));
-
-var usersController = require('./controllers/users.js');
-app.use('/users', usersController);
-
-var sessionsController = require('./controllers/sessions.js');
-app.use('/sessions', sessionsController);
-
-app.get('/', function(req, res){
-    res.render('index.ejs', {
-        currentUser: req.session.currentuser
-    });
-});
-
-app.get('/app', function(req, res){
-    if(req.session.currentuser){
-        res.send('the party');
-    } else {
-        res.redirect('/sessions/new');
-    }
-});
+//
+//
+// app.use(methodOverride('_method'));
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(session({
+//     secret: "feedmeseymour", //some random string
+//     resave: false,
+//     saveUninitialized: false
+// }));
+//
+// var usersController = require('./controllers/users.js');
+// app.use('/users', usersController);
+//
+// var sessionsController = require('./controllers/sessions.js');
+// app.use('/sessions', sessionsController);
+//
+// app.get('/', function(req, res){
+//     res.render('index.ejs', {
+//         currentUser: req.session.currentuser
+//     });
+// });
+//
+// app.get('/app', function(req, res){
+//     if(req.session.currentuser){
+//         res.send('the party');
+//     } else {
+//         res.redirect('/sessions/new');
+//     }
+// });
 
 app.listen(port, function(){
   console.log('=================================================');
